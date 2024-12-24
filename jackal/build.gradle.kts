add1445a9c98f702099231448a1496b7ca3dd29d
hongyi <develop@yifeistudio.com>
2024-12-24 16:39:28 +0800

    publish alps-security java library: unit test.

--- jackal/build.gradle.kts
+++ jackal/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("io.grpc:grpc-testing:1.69.0")
     implementation("io.grpc:grpc-protobuf:1.69.0")
     implementation("io.grpc:grpc-inprocess:1.69.0")
-    implementation("io.grpc:grpc-kotlin-stub:1.4.1")
+    api("io.grpc:grpc-kotlin-stub:1.4.1")
     implementation("com.google.protobuf:protobuf-java:4.29.1")
     testImplementation(kotlin("test"))
 }
